a705022418449dc276f1e630c7097a4ec0475da5
Nedj <106325998+Nedj78@users.noreply.github.com>
2024-03-04 16:02:57 +0100

    update.js

--- script.js
+++ script.js
@@ -34,3 +34,88 @@ setupConversionListeners("minutes2", "minute", "heure", "result2", 1/60);
 setupConversionListeners("hours3", "heure", "jour", "result3", 1/24);
 
 setupConversionListeners("days4", "jour", "heure", "result4", 24);
+
+// ------------------------------------------------------------------
+
+let display = document.querySelector('.display');
+let scratchPad = document.querySelector('.scratch-pad');
+let scratchPadText = [];
+let lastResult = null;
+
+function appendToDisplay(value) {
+    if (lastResult !== null) {
+        display.innerHTML = '';
+        lastResult = null;
+    }
+    display.innerHTML += value;
+    updateScratchPad();
+}
+
+function clearDisplay() {
+    display.innerHTML = '';
+    scratchPadText = [];
+    updateScratchPad();
+}
+
+function backspace() {
+    display.innerHTML = display.innerHTML.slice(0, -1);
+    updateScratchPad();
+}
+
+function evaluateExpression() {
+    let expression = display.innerHTML;
+    let result = eval(expression);
+    display.innerHTML = result;
+    if (lastResult === null) {
+        scratchPadText.push(expression + ' = ' + result);
+    } else {
+        scratchPadText.push(lastResult + ' ' + expression + ' = ' + result);
+    }
+    lastResult = result;
+    updateScratchPad();
+}
+
+function percentage() {
+    let expression = display.innerHTML;
+    let result = eval(expression) / 100;
+    display.innerHTML = result;
+    scratchPadText.push(expression + '% = ' + result);
+    updateScratchPad();
+}
+
+function updateScratchPad() {
+    let scratchPadHTML = '<h2>Tableau de brouillon</h2>';
+    scratchPadText.forEach(function(item) {
+        scratchPadHTML += '<br>' + '<p>' + item + '</p>' + '<br>';
+    });
+    scratchPad.innerHTML = scratchPadHTML;
+}
+
+// Configuration des boutons de la calculatrice
+const calculatorButtons = document.querySelectorAll('.buttoncalculator');
+calculatorButtons.forEach(button => {
+    button.addEventListener('click', function() {
+        appendToDisplay(button.textContent);
+    });
+});
+
+// Configuration du bouton C (effacer)
+document.getElementById('clearButton').addEventListener('click', clearDisplay);
+
+// Configuration du bouton ⌦ (retour arrière)
+document.getElementById('backspaceButton').addEventListener('click', backspace);
+
+// Configuration du bouton = (évaluation)
+document.getElementById('evaluateButton').addEventListener('click', evaluateExpression);
+
+// Configuration du bouton % (pourcentage)
+document.getElementById('percentageButton').addEventListener('click', percentage);
+
+// Configuration du bouton Entrée (évaluation)
+document.addEventListener('keydown', function(event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        evaluateExpression();
+    }
+});
+
